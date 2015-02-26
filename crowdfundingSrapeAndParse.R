f7bf129ad0ee4a17e45f1f37c0b80527587c75d6
basbaccarne <bastiaan.baccarne@ugent.be>
2015-02-26 19:11:31 +0100

    first experiments - listing the projects

---
+++ crowdfundingSrapeAndParse.R
@@ -0,0 +1,25 @@
+## needs R package "ScrapeR"
+library (scrapeR)
+
+## read overview of platforms
+platforms <- read.csv("../Google Drive/onderzoek - projecten/2015 Urban crowdfunding/platforms.csv", header=TRUE, sep=";")
+
+## loop through platforms + error handling
+for(i in 1:nrow(platforms)){
+  tryCatch({
+    
+## voorjebuurt
+      if (toString(platforms[i, 1])=="voorjebuurt"){
+            platformurl <- toString(platforms[i, 3])    
+            ##testing in -  enkel voor je buurt##
+            platformurl = toString(platforms[2,3])
+            ##testing out - enkel voor je buurt##
+            pagesource <- readLines(platformurl)
+            pagesource.raw <- htmlTreeParse(pagesource, useInternalNodes = T)
+            links <- as.vector(xpathSApply(pagesource.raw, "//h3//a/@href"))
+      
+      } else {
+            print (toString(platforms[i, 1]))
+      }
+  })
+}
